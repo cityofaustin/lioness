0c89b2990e74cd585145d18da6216b57cfef4a2c
Brian Smith <briaguya@gmail.com>
2018-06-18 00:07:51 -0500

    little bit of code to start populating the db

---
+++ data/src/CreateThreeOneOne.js
@@ -0,0 +1,19 @@
+import { request } from "graphql-request";
+
+const CreateThreeOneOne = async (endpoint, title, url) => {
+  const query = `
+    mutation($title:String!, $url:String!) {
+      createThreeOneOne(title:$title, url:$url) {
+        id
+      }
+    }
+  `;
+  const variables = {
+    title,
+    url
+  };
+  const data = await request(endpoint, query, variables);
+  return data;
+};
+
+export default CreateThreeOneOne;
---
+++ data/src/populateData.js
@@ -0,0 +1,7 @@
+import "babel-polyfill";
+import CreateThreeOneOne from "./CreateThreeOneOne";
+
+const gqlEndpoint =
+  "http://localhost:60000/simple/v1/cjijrudcn000601688d7k96zl";
+
+CreateThreeOneOne(gqlEndpoint, "blarg", "blarg");
