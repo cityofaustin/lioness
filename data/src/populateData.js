a2d8583589d05bc7a4cf77f2550c38e39da3dc84
Brian Smith <briaguya@gmail.com>
2018-06-18 00:37:33 -0500

    add fixtures files and add 311s from there

--- data/src/populateData.js
+++ data/src/populateData.js
@@ -1,7 +1,21 @@
 import "babel-polyfill";
+import fs from "fs";
+import yaml from "js-yaml";
+
 import CreateThreeOneOne from "./CreateThreeOneOne";
 
 const gqlEndpoint =
-  "http://localhost:60000/simple/v1/cjijrudcn000601688d7k96zl";
+  "http://localhost:60000/simple/v1/cjijth3m700040192biiapqvu";
 
-CreateThreeOneOne(gqlEndpoint, "blarg", "blarg");
+// Add 311
+try {
+  const threeOneOnes = yaml.safeLoad(
+    fs.readFileSync("./data/fixtures/311.yaml", "utf8")
+  );
+  // console.log(threeOneOnes["311"]);
+  threeOneOnes["311"].forEach(threeOneOne =>
+    CreateThreeOneOne(gqlEndpoint, threeOneOne.title_en, threeOneOne.url)
+  );
+} catch (e) {
+  console.log(e);
+}
